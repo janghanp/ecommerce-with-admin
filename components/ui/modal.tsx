288c015dafe97271f205515cc5e0c190da252e1d
janghan0412@gmail.com <janghan0412@gmail.com>
2023-06-30 14:39:21 +1000

    Add authentication with clerk

---
+++ components/ui/modal.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog";
+
+interface Props {
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+}
+
+const Modal = ({ title, description, isOpen, onClose, children }: Props) => {
+    const onChange = (isOpen: boolean) => {
+        if (!isOpen) {
+            onClose();
+        }
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {title}
+                    </DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                <div>
+                    {children}
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default Modal;
