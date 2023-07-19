bfffeb936afafc16e10cc129f15185d45e5c74ad
janghan0412@gmail.com <janghan0412@gmail.com>
2023-07-19 23:11:27 +1000

    Update user info

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -1,44 +1,40 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useClerk, UserButton, useUser } from "@clerk/nextjs";
+import { UserButton, SignOutButton } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
 
 import { Skeleton } from "@/src/components/ui/skeleton";
 
 const UserInfo = () => {
-    const { signOut } = useClerk();
-    const { isLoaded, isSignedIn, user } = useUser();
-
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    if (!isClient) {
-        return;
-    }
-
-    if (!isLoaded || !isSignedIn) {
-        return (
-            <>
-                <Skeleton className="h-8 w-full" />
-            </>
-        );
-    }
+    // const { signOut } = useClerk();
+    // const { isLoaded, isSignedIn, user } = useUser();
+    //
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     setIsClient(true);
+    // }, []);
+    //
+    // if (!isClient) {
+    //     return;
+    // }
+    //
+    // if (!isLoaded || !isSignedIn) {
+    //     return (
+    //         <>
+    //             <Skeleton className="h-8 w-full" />
+    //         </>
+    //     );
+    // }
 
     return (
         <div className="ml-auto flex w-full items-center space-x-4">
             <div className="flex w-full items-center justify-between gap-x-2 p-2 py-1">
                 <UserButton afterSignOutUrl="/" />
-                <span className="text-sm font-medium">{user?.firstName}</span>
-                <div
-                    className="rounded-md p-2 transition duration-200 hover:cursor-pointer hover:bg-gray-200"
-                    onClick={() => signOut()}
-                >
-                    <LogOut className="h-4 w-4" />
-                </div>
+                {/*<span className="text-sm font-medium">{user?.firstName}</span>*/}
+                <SignOutButton>
+                    <div className="rounded-md p-2 transition duration-200 hover:cursor-pointer hover:bg-gray-200">
+                        <LogOut className="h-4 w-4" />
+                    </div>
+                </SignOutButton>
             </div>
         </div>
     );
