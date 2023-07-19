b9d2cee1fd503f2a282c55c421f830620aca30c4
janghan0412@gmail.com <janghan0412@gmail.com>
2023-07-19 23:36:35 +1000

    Add skeleton in user info

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -1,35 +1,26 @@
-import { UserButton, SignOutButton } from "@clerk/nextjs";
+"use client";
+
+import { UserButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
 
 import { Skeleton } from "@/src/components/ui/skeleton";
 
 const UserInfo = () => {
-    // const { signOut } = useClerk();
-    // const { isLoaded, isSignedIn, user } = useUser();
-    //
-    // const [isClient, setIsClient] = useState(false);
-    //
-    // useEffect(() => {
-    //     setIsClient(true);
-    // }, []);
-    //
-    // if (!isClient) {
-    //     return;
-    // }
-    //
-    // if (!isLoaded || !isSignedIn) {
-    //     return (
-    //         <>
-    //             <Skeleton className="h-8 w-full" />
-    //         </>
-    //     );
-    // }
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    if (!isLoaded || !isSignedIn) {
+        return (
+            <>
+                <Skeleton className="h-8 w-full" />
+            </>
+        );
+    }
 
     return (
         <div className="ml-auto flex w-full items-center space-x-4">
             <div className="flex w-full items-center justify-between gap-x-2 p-2 py-1">
                 <UserButton afterSignOutUrl="/" />
-                {/*<span className="text-sm font-medium">{user?.firstName}</span>*/}
+                <span className="text-sm font-medium">{user?.firstName}</span>
                 <SignOutButton>
                     <div className="rounded-md p-2 transition duration-200 hover:cursor-pointer hover:bg-gray-200">
                         <LogOut className="h-4 w-4" />
