3c0415f1331484ee203345c18eb03a8be5fb0669
janghan0412@gmail.com <janghan0412@gmail.com>
2023-07-19 22:59:00 +1000

    Update user info

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -7,25 +7,25 @@ import { Skeleton } from "@/src/components/ui/skeleton";
 
 const UserInfo = () => {
     const { signOut } = useClerk();
-    const { isLoaded, isSignedIn, user } = useUser();
-
-    console.log(isLoaded);
-    console.log(isSignedIn);
-    console.log(user);
-
-    if (!isLoaded || !isSignedIn) {
-        return (
-            <>
-                <Skeleton className="h-8 w-full" />
-            </>
-        );
-    }
+    // const { isLoaded, isSignedIn, user } = useUser();
+    //
+    // console.log(isLoaded);
+    // console.log(isSignedIn);
+    // console.log(user);
+    //
+    // if (!isLoaded || !isSignedIn) {
+    //     return (
+    //         <>
+    //             <Skeleton className="h-8 w-full" />
+    //         </>
+    //     );
+    // }
 
     return (
         <div className="ml-auto flex w-full items-center space-x-4">
             <div className="flex w-full items-center justify-between gap-x-2 p-2 py-1">
                 <UserButton afterSignOutUrl="/" />
-                <span className="text-sm font-medium">{user?.firstName}</span>
+                <span className="text-sm font-medium">{}</span>
                 <div
                     className="rounded-md p-2 transition duration-200 hover:cursor-pointer hover:bg-gray-200"
                     onClick={() => signOut()}
